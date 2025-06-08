514fbd47e1443e3e5a69fef7703a6b3f48b95889
Kadiss Ibrahim <ka.ibrahim2004@gmail.com>
2025-06-08 04:04:34 +0100

    changement dans Tache controller pour difficulte et priorite, et traduire reste en francais

--- src/main/java/gui/javafrontend/AjouterTacheController.java
+++ src/main/java/gui/javafrontend/AjouterTacheController.java
@@ -1,20 +1,19 @@
 package gui.javafrontend;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
-import java.util.HashMap;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static utils.Validation.showSuccess;
 
-public class AjouterTacheController {
+public class AjouterTacheController implements Initializable {
 
     @FXML
     private Label ajouterSucces;
@@ -29,19 +28,16 @@ public class AjouterTacheController {
     private TextArea descriptionField;
 
     @FXML
-    private Spinner<?> difficulteSpinner;
+    private Spinner<String> difficulteSpinner;
 
     @FXML
     private ComboBox<?> etatComboBox;
 
     @FXML
     private ImageView image2;
-
+    
     @FXML
-    private Spinner<?> notationSpinner;
-
-    @FXML
-    private Spinner<?> prioriteSpinner;
+    private Spinner<Integer> prioriteSpinner;
 
     @FXML
     private TextField titreField;
@@ -49,6 +45,22 @@ public class AjouterTacheController {
     @FXML
     void ajouterTache(ActionEvent event) {
         showSuccess("Tache bien Ajouter ",ajouterSucces);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        SpinnerValueFactory<Integer> valueFactory =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5, 1);
+        prioriteSpinner.setValueFactory(valueFactory);
+        ObservableList<String> niveaux = FXCollections.observableArrayList("Facile", "Moyenne", "Difficile"
+        );
+
+        SpinnerValueFactory<String> valFactory =
+                new SpinnerValueFactory.ListSpinnerValueFactory<>(niveaux);
+        valFactory.setValue("Facile");
+
+        difficulteSpinner.setValueFactory(valFactory);
     }
 
 }
